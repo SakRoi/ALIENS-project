e52da8a889f0ed941ce76a073111ff9610d363ac
SakRoi <roininensaku@gmail.com>
2023-02-06 18:22:16 +0200

    added helper methods for both left and right movement

--- ALIENS.py
+++ ALIENS.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events
             self._check_events()
+            self.ship.update()
             self._update_screen()
     
     def _check_events(self) -> None:
@@ -30,10 +31,29 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+            else:
+                self._right_movement_checker(event)
+                self._left_movement_checker(event)
+
+    def _right_movement_checker(self, event) -> None:
+        """Helper function for moving the spaceship right"""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # Move the ship to the right.
+                self.ship.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+    
+    def _left_movement_checker(self, event) -> None:
+        """Helper function for moving the spaceship right"""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                # Move the ship to the left.
+                self.ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
 
     
     def _update_screen(self) -> None:
