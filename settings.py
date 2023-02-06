a32b2f5822e40d732cf5daeb96af72cd8f5465ec
SakRoi <roininensaku@gmail.com>
2023-02-06 17:05:40 +0200

    changed the color into a blue

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ class Settings:
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.background_color = (230, 230, 230)
+        self.background_color = (0, 0, 100)
