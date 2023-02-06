67bdac75f3483683f0c6f07bed2f2354bc3a4856
SakRoi <roininensaku@gmail.com>
2023-02-06 18:53:14 +0200

    added fullscreen option

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #Screen settings
+        self.fullscreen = False
         self.screen_width = 1200
         self.screen_height = 800
         self.background_color = (0, 0, 100)
