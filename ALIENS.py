d52c51abfbfe8ec48e76e95662edf4ce23ace727
SakRoi <roininensaku@gmail.com>
2023-02-06 17:41:01 +0200

    Moved event listener to it's own helper function _check_events

--- ALIENS.py
+++ ALIENS.py
@@ -18,19 +18,23 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-    def run_game(self):
+    def run_game(self) -> None:
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             # Redraws the screen during each pass though the loop
             self.screen.fill(self.settings.background_color)
             self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+    
+    def _check_events(self) -> None:
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
