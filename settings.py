71e0790e2f5cb1082d7c17a705c310ac19454ec5
SakRoi <roininensaku@gmail.com>
2023-02-06 16:47:15 +0200

    Created settings to make changing the games settings easier. refactored the game to use the settings file.

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+class Settings:
+    """A class that stores all the settings for ALIENS"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.background_color = (230, 230, 230)
