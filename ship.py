a1d93aeee083a6151ffd34c032ff543ff7d1d409
SakRoi <roininensaku@gmail.com>
2023-02-06 17:06:47 +0200

    added more functionality and added type hints to the methods where appliable

--- ship.py
+++ ship.py
@@ -3,11 +3,18 @@ import pygame
 class Ship:
     """A simple class to represent an alien spaceship"""
 
-    def __init__ (self, ai_game):
+    def __init__ (self, ai_game) -> None:
         """Intialize the ship and set its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         
         #load the ship image and get it's rect.
         self.image = pygame.image.load("images/ship.bmp")
-        
+        self.rect = self.image.get_rect()
+        #Starts the ship at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+    
+    def blitme(self) -> None:
+        """Draw the ship at it's current location."""
+        self.screen.blit(self.image, self.rect)
+
