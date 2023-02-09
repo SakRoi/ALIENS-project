696fafa5126d5bd083f3574995a1e4ba01a5a938
SakRoi <roininensaku@gmail.com>
2023-02-09 19:19:32 +0200

    added settings for the bullets, added .gitignore for the image file for now

--- settings.py
+++ settings.py
@@ -5,10 +5,17 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #Screen settings
-        self.fullscreen = False
+        self.fullscreen = True
         self.screen_width = 1200
         self.screen_height = 800
         self.background_color = (0, 0, 100)
         
         #Ship's settings
         self.ship_speed = 1.5
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color(60,60,60)
+        
