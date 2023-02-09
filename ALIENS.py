a2f2c4cfe58b195b482e4e4269dc16d6e4c4c242
SakRoi <roininensaku@gmail.com>
2023-02-09 19:35:51 +0200

    added bullet class to the ALIENS

--- ALIENS.py
+++ ALIENS.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -22,6 +23,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self) -> None:
         """Start the main loop for the game"""
@@ -29,6 +31,7 @@ class AlienInvasion:
             # Watch for keyboard and mouse events
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
     
     def _check_events(self) -> None:
