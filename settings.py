b0999e34efafd287f3d98b5ec83cf8766b7bd9d0
SakRoi <roininensaku@gmail.com>
2023-02-12 14:53:35 +0200

    added new settings for alien space ships

--- settings.py
+++ settings.py
@@ -12,6 +12,9 @@ class Settings:
 
         #Alien's settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right, -1 represents left
+        self.fleet_direction = 1
         
         #Ship's settings
         self.ship_speed = 1.5
