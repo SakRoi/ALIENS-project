107b6a096f0e985ef4172ec55690c23b5e0255f2
SakRoi <roininensaku@gmail.com>
2023-02-19 12:41:19 +0200

    Feat: Add code to center the button on the game screen

--- button.py
+++ button.py
@@ -12,4 +12,9 @@ class Button:
         self.button_color = (0, 255, 0)
         self.text_color = (255,255,255)
         self.font = pygame.font.SysFont(None, 48)
-        
+
+        #Builds the buttons rect object
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        self._prep_msg(msg)
